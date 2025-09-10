2d8faa633e8abfe9150bc29c54958f848ac44435
jane-spothub <techspothubke@gmail.com>
2025-09-10 16:48:57 +0300

    updated bethistory

--- src/Components/BetHistoryTable.tsx
+++ src/Components/BetHistoryTable.tsx
@@ -1,16 +1,16 @@
-import type { BetHistoryItem } from "../Utils/types.ts";
+import type {BetHistoryItem} from "../Utils/types.ts";
 
 interface Props {
     history: BetHistoryItem[];
 }
 
-export const BetHistoryTable = ({ history }: Props) => {
-    const getResultStyle = (result: string) => {
-        if (result === "0") return { color: "#e53e3e", fontWeight: "bold" };
-        if (result.includes("K") || !isNaN(Number(result))) return { color: "#ffaf00", fontWeight: "bold" };
-        if (result.includes("7,500")) return { color: "#4fd1c5", fontWeight: "bold" };
-        if (result.includes("Gonga")) return { color: "#6ffd00", fontWeight: "bold" };
-        return { color: "#e2e8f0" };
+export const BetHistoryTable = ({history}: Props) => {
+    const getResultStyle = (result: string, betAmount: string) => {
+        if (betAmount === "Nunge Tosha") return {color: "#e53e3e", fontWeight: "bold"};
+        if (result.includes("K") || !isNaN(Number(result))) return {color: "#ffaf00", fontWeight: "bold"};
+        if (result.includes("7,500")) return {color: "#4fd1c5", fontWeight: "bold"};
+        if (result.includes("Gonga")) return {color: "#6ffd00", fontWeight: "bold"};
+        return {color: "#e2e8f0"};
     };
 
     // 👇 Map multiplier strings to kes
@@ -22,6 +22,8 @@ export const BetHistoryTable = ({ history }: Props) => {
                 return "49 kes";
             case "4x":
                 return "99 kes";
+            case "x":
+                return "20 kes";
             default:
                 return betAmount; // fallback if something else comes
         }
@@ -32,25 +34,34 @@ export const BetHistoryTable = ({ history }: Props) => {
             <h2 className="bet-title">Bet History</h2>
             <table className="bet-history-spin">
                 <thead>
-                <tr style={{ background: "#202733" }}>
+                <tr style={{background: "#202733"}}>
                     <th className="history-bet-titles">Bet Amount</th>
                     <th className="history-bet-titles">Result</th>
                 </tr>
                 </thead>
                 <tbody>
-                {history.map((item, index) => (
-                    <tr
-                        key={item.id}
-                        style={{ background: index % 2 === 0 ? "#080a0e" : "#191f2a" }}
-                    >
-                        <td className="bet-item">{formatBetAmount(item.betAmount)}</td>
-                        <td className="bet-item" style={{ ...getResultStyle(item.result) }}>
-                            {item.result}
-                        </td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
-    );
+                {history.map((item, index) => {
+                    const result = item.result === "Nunge Tosha" ? "0"
+                        : ( item.result === "SPIN TENA" ?"1 Free Spin"
+                            :( item.result === "Zako 3" ?"3 Free Spins":
+                                ( item.result === "Zako 2" ?"2 Free Spins"
+                                    :item.result)));
+                    return (
+                        <tr
+                            key={item.id}
+                            style={{background: index % 2 === 0 ? "#080a0e" : "#191f2a"}}
+                        >
+                            <td className="bet-item">{formatBetAmount(item.betAmount)}</td>
+                            <td className="bet-item"
+                                style={{...getResultStyle(item.result, item.betAmount)}}>
+                                {result}
+                            </td>
+                        </tr>
+                    )
+                } )}
+            </tbody>
+        </table>
+</div>
+)
+    ;
 };
